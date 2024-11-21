e45350ee1810a4d2303c780ce6dae2b6cfb499ed
Isaac Hager <isaachager@u.boisestate.edu>
2024-11-20 19:12:42 -0700

    Comments working (No deletion or updates)

--- src/server.js
+++ src/server.js
@@ -6,6 +6,10 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
+db.run(`CREATE TABLE student2Comments (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  message TEXT NOT NULL)`)  
+
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
@@ -22,9 +26,51 @@ app.get('/student1', function (req, res) {
   res.render('student1')
 })
 
+// Get comments from database and render index with results
 app.get('/student2', function (req, res) {
   console.log('GET called')
-  res.render('student2')
+  const local = { comments: [] }
+        db.each('SELECT id, message FROM student2Comments', function (err, row) {
+            if (err) {
+                console.log(err)
+            } else {
+                local.comments.push({ id: row.id, message: row.message })
+            }
+        }, function (err, numrows) {
+            if (!err) {
+                res.render('student2/index', local)
+            } else {
+                console.log(err)
+            }
+        })
+})
+
+// Get comments from database and render comments with results
+app.get('/student2/comments', function (req, res) {
+  console.log('GET called')
+  const local = { comments: [] }
+        db.each('SELECT id, message FROM student2Comments', function (err, row) {
+            if (err) {
+                console.log(err)
+            } else {
+                local.comments.push({ id: row.id, message: row.message })
+            }
+        }, function (err, numrows) {
+            if (!err) {
+                res.render('student2/comments', local)
+            } else {
+                console.log(err)
+            }
+        })
+})
+
+// Post comments to the database
+app.post('/student2/comments', function (req, res) {
+  console.log('adding comment')
+  const stmt = db.prepare('INSERT INTO student2Comments (message) VALUES (?)')
+  stmt.run(req.body.comment)
+  stmt.finalize()
+  res.redirect("/student2/comments")
 })
 
 app.get('/student3', function (req, res) {
