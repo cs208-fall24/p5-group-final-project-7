b04578d25fa71bccfb8c2b672c39ccc997ed66c4
QuintonDuce <quintonduce@u.boisestate.edu>
2024-11-20 21:55:05 -0700

    Backend functioning (Needs Frontend implementation)

--- src/server.js
+++ src/server.js
@@ -6,6 +6,13 @@ const sqlite3 = sql.verbose()
 // Create an in memory table to use
 const db = new sqlite3.Database(':memory:')
 
+db.serialize(() => {   
+  db.run(`CREATE TABLE student1comments (
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      message TEXT NOT NULL)`);
+});
+
+
 const app = express()
 app.use(express.static('public'))
 app.set('views', 'views')
@@ -19,7 +26,29 @@ app.get('/', function (req, res) {
 
 app.get('/student1', function (req, res) {
   console.log('GET called')
-  res.render('student1')
+  const local = {comments: []}
+    db.each('SELECT id, message FROM student1comments', function (err, row) {
+      if (err) {
+        console.error(err)
+      } else {
+        local.comments.push({id: row.id, message: row.message})
+      }
+    }, function (err, numrows) {
+      if(!err){
+        res.render('student1/comments', local)
+      } else {
+        console.log(err)
+      }
+    });
+
+});
+
+app.post('/student1/comments', function (req, res) {
+  console.log('adding comment')
+  const stmt = db.prepare('INSERT INTO student1Comments (message) VALUES (?)')
+  stmt.run(req.body.comment)
+  stmt.finalize()
+  res.redirect("/student1/comments")
 })
 
 app.get('/student2', function (req, res) {
